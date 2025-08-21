624f6627342ee02a00c5d949a0ee6cc287c6fc08
jesse sheehan <jessetsheehan@gmail.com>
2025-08-21 11:26:27 -0400

    First Init to set up repo

---
+++ MineSeeker/MineSeeker/MineSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MineSeekerApp.swift
+//  MineSeeker
+//
+//  Created by Jesse Sheehan on 8/21/25.
+//
+
+import SwiftUI
+
+@main
+struct MineSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
