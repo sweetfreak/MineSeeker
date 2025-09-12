491d33d43f691ecda8444efb998c0cf324cdd349
jesse sheehan <jessetsheehan@gmail.com>
2025-09-12 16:59:29 -0400

    Fixed Instructions

--- MineSeeker/MineSeeker/Views/MenuViews/InstructionsView.swift
+++ MineSeeker/MineSeeker/Views/MenuViews/InstructionsView.swift
@@ -9,8 +9,8 @@ import SwiftUI
 
 struct InstructionsView: View {
     @State private var demoTileBomb = Tile(row: 1, column: 1, isMine: true, isRevealed: true)
-    @State private var demoTileFlag = Tile(row: 1, column: 1, isMine: true, isRevealed: false, isFlagged: true)
-    @State private var demoTileReveal = Tile(row: 1, column: 1, isMine: false, )
+//    @State private var demoTileFlag = Tile(row: 1, column: 1, isMine: true, isRevealed: false, isFlagged: true)
+    @State private var demoTileReveal = Tile(row: 1, column: 1, isMine: false, surroundingMineCount: 1)
     
     @State var vm: FieldViewModel
     
@@ -26,40 +26,38 @@ struct InstructionsView: View {
                     Text("Objective:")
                         .bold()
                     HStack{
-                        Text("Use flags to mark each tile that contains a mine.")
-                        //                        Spacer()
-                        //                        TileView(tile: $demoTileBomb, vm: vm)
+                        Text("Find all the mines and mark each one with a flag.")
                     }
                 }
-                
+                Spacer()
             }
-            .padding(5)
+            .padding(EdgeInsets(top: 0, leading: 0, bottom: 10, trailing: 0))
             
-            
-            
+ 
             
             HStack {
                 VStack(alignment: .leading) {
                     Text("Directions:")
                         .bold()
+                    
                     HStack {
-                        Text("If you tap on a tile that has a mine, you lose.")
+                        Text("Tap on a tile to reveal what's underneath. The revealed number indicates how many surrounding tiles are mines.")
+                        Spacer()
+                        TileView(tile: $demoTileReveal, vm: vm, )
+                            .font(.title)
+                    }
+                    Divider()
+                    
+                    HStack {
+                        Text("If you tap on a mine tile, it explodes.\n(Which is bad)")
                         Spacer()
                         TileView(tile: $demoTileBomb, vm: vm)
                             .font(.title)
                     }
-                    
+                    Divider()
+
                     HStack {
-                        Text("Tap to reveal a tile.")
-                        Spacer()
-                        TileView(tile: $demoTileReveal, vm: vm)
-                            .font(.title)
-                    }
-                    
-                    HStack {
-                        
-                        Text("To flag a mine, hold and drag the flag over to the tile.")
-                        Spacer()
+                        Text("Mark each mine by dragging a flag over to the tile.")
                         ZStack {
                             Circle()
                                 .fill(Color("tileBack"))
@@ -69,12 +67,11 @@ struct InstructionsView: View {
                                 .resizable()
                                 .frame(width: 50, height: 50)
                         }
-                        //TileView(tile: $demoTileFlag, vm: vm)
-                        
                     }
-                    
+                    Divider()
+
                     HStack {
-                        Text("To remove move a flag from a tile, hold the shovel and drag it to the flag.")
+                        Text("To remove a flag from a tile, drag and drop the shovel over the flag.")
                         Spacer()
                         ZStack {
                             Circle()
@@ -85,9 +82,27 @@ struct InstructionsView: View {
                                 .frame(width: 50, height: 50)
                         }
                     }
+                    
+                    Divider()
+                    VStack(alignment: .leading){
+                        HStack {
+                            Text("Tap the check button if you think you've flagged all the mines. ")
+                            Spacer()
+                            Button {} label: {
+                                Label("Check", systemImage: "checkmark.circle.fill")
+                                
+                                    .symbolEffect(.bounce)
+                            }
+                            //.buttonStyle(.glassProminent)
+                            .buttonStyle(.borderedProminent)
+                        }
+                        Text("Incorrect flag placement will deduct 500 points")
+                            .font(.caption2)
+                            
+                    }
+
                 }
             }
-            .padding()
             
             if vm.gameStarted {
                 Button{
@@ -95,11 +110,14 @@ struct InstructionsView: View {
             } label: {
                 Label("Back", systemImage: "arrow.left")
             }
+            .padding()
                 
             } else {
                 HomeButtonView(vm: vm)
+                    .padding()
             }
         }
+        .padding(10)
         
     }
 }
